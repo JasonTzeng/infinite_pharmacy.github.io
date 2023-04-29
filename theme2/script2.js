2c5347b27755cfbd077c6b93282540d6c66862a8
JHsiang <a0983069681@gmail.com>
2023-04-30 00:32:35 +0800

    back and next button

--- theme2/script2.js
+++ theme2/script2.js
@@ -1,58 +1,112 @@
 $(".button").click(function () {
-    var $btn = $(this),
-      $step = $btn.parents(".modal-body"),
-      stepIndex = $step.index(),
-      $pag = $(".modal-header span").eq(stepIndex);
-  
-    if (stepIndex === 0 || stepIndex === 1) {
-      step1($step, $pag);
-    } else {
-      step3($step, $pag);
-    }
-  });
-  
-  function step1($step, $pag) {
-    console.log("step1");
-    // animate the step out
-    $step.addClass("animate-out");
-  
-    // animate the step in
-    setTimeout(function () {
-      $step.removeClass("animate-out is-showing").next().addClass("animate-in");
-      $pag.removeClass("is-active").next().addClass("is-active");
-    }, 600);
-  
-    // after the animation, adjust the classes
-    setTimeout(function () {
-      $step.next().removeClass("animate-in").addClass("is-showing");
-    }, 1200);
+  var $btn = $(this),
+    $step = $btn.parents(".modal-body"),
+    stepIndex = $step.index(),
+    $pag = $(".modal-header span").eq(stepIndex);
+
+  if (stepIndex === 0) {
+    stepNext($step, $pag);
+  } else if (stepIndex === 1) {
+    stepBack($step, $pag);
   }
-  
-  function step3($step, $pag) {
-    console.log("3");
-  
-    // animate the step out
-    $step.parents(".modal-wrap").addClass("animate-up");
-  
-    setTimeout(function () {
-      $(".rerun-button").css("display", "inline-block");
-    }, 300);
+  else {
+    step3($step, $pag);
   }
-  
-  $(".rerun-button").click(function () {
-    $(".modal-wrap")
-      .removeClass("animate-up")
-      .find(".modal-body")
-      .first()
-      .addClass("is-showing")
-      .siblings()
-      .removeClass("is-showing");
-  
-    $(".modal-header span")
-      .first()
-      .addClass("is-active")
-      .siblings()
-      .removeClass("is-active");
-    $(this).hide();
-  });
-  
+});
+
+$(".buttonNext").click(function () {
+  var $btn = $(this),
+    $step = $btn.parents(".modal-body"),
+    stepIndex = $step.index(),
+    $pag = $(".modal-header span").eq(stepIndex);
+
+  stepNext($step, $pag);
+});
+
+$(".buttonBack").click(function () {
+  var $btn = $(this),
+    $step = $btn.parents(".modal-body"),
+    stepIndex = $step.index(),
+    $pag = $(".modal-header span").eq(stepIndex);
+
+  stepBack($step, $pag);
+});
+
+
+function step1($step, $pag) {
+  console.log("step1");
+  // animate the step out
+  $step.addClass("animate-out");
+
+  // animate the step in
+  setTimeout(function () {
+    $step.removeClass("animate-out is-showing").next().addClass("animate-in");
+    $pag.removeClass("is-active").next().addClass("is-active");
+  }, 600);
+
+  // after the animation, adjust the classes
+  setTimeout(function () {
+    $step.next().removeClass("animate-in").addClass("is-showing");
+  }, 1200);
+}
+
+function step3($step, $pag) {
+  console.log("3");
+
+  // animate the step out
+  $step.parents(".modal-wrap").addClass("animate-up");
+
+  setTimeout(function () {
+    $(".rerun-button").css("display", "inline-block");
+  }, 300);
+}
+
+function stepNext($step, $pag) {
+  // animate the current step out
+  $step.addClass("animate-out");
+
+  // animate the next step in
+  setTimeout(function () {
+    $step.removeClass("animate-out is-showing").next().addClass("animate-in");
+    $pag.removeClass("is-active").next().addClass("is-active");
+  }, 600);
+
+  // after the animation, adjust the classes
+  setTimeout(function () {
+    $step.next().removeClass("animate-in").addClass("is-showing");
+  }, 1200);
+}
+
+function stepBack($step, $pag) {
+  // animate the current step out
+  $step.addClass("animate-out");
+
+  // animate the previous step in
+  setTimeout(function () {
+    $step.removeClass("animate-out is-showing").prev().addClass("animate-in");
+    $pag.removeClass("is-active").prev().addClass("is-active");
+  }, 600);
+
+  // after the animation, adjust the classes
+  setTimeout(function () {
+    $step.prev().removeClass("animate-in").addClass("is-showing");
+  }, 1200);
+}
+
+
+$(".rerun-button").click(function () {
+  $(".modal-wrap")
+    .removeClass("animate-up")
+    .find(".modal-body")
+    .first()
+    .addClass("is-showing")
+    .siblings()
+    .removeClass("is-showing");
+
+  $(".modal-header span")
+    .first()
+    .addClass("is-active")
+    .siblings()
+    .removeClass("is-active");
+  $(this).hide();
+});
